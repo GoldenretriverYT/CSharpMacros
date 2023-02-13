9bbb937684fb07c2aa90cd20d22b0a935eb6a69f
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-02-13 21:46:51 +0100

    Initial commit

---
+++ CSharpMacros/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CSharpMacros {
+    internal class Program {
+        static void Main(string[] args) {
+            if (args.Length != 1) {
+                Console.WriteLine("Usage: CSharpMacros.exe <path to file>");
+                return;
+            }
+
+            var path = args[0];
+
+            if (!File.Exists(path)) {
+                Console.WriteLine($"File {path} does not exist");
+                return;
+            }
+
+            var text = File.ReadAllText(path);
+            string[] lines = text.Split(Environment.NewLine);
+            var macros = new Dictionary<string, (List<string> parameters, string body)>();
+
+            for(int i = 0; i < lines.Length; i++) {
+                var line = lines[i];
+                if (line.StartsWith("/*macro ")) {
+                    List<string> parameters = line.Split(" ").Skip(1).ToList();
+                    string name = parameters[0];
+
+                    parameters.RemoveAt(0);
+                    string body = "";
+                    while (i < lines.Length-1) {
+                        i++;
+                        line = lines[i];
+
+                        if (line.EndsWith("*/"))
+                            break;
+                        
+                        body += line + Environment.NewLine;
+                    }
+
+                    macros.Add(name, (parameters, body));
+                }
+            }
+
+            // remove everything between #region MacroDummies and #endregion
+            text = Regex.Replace(text, @"#region MacroDummies.*#endregion", "", RegexOptions.Singleline);
+
+            foreach (var macro in macros) {
+                var name = macro.Key;
+                var parameters = macro.Value.parameters;
+                var body = macro.Value.body;
+
+                var regex = new Regex($"\\b{name}\\b\\s*\\((.*?)\\)");
+                var matches = regex.Matches(text);
+
+                foreach (Match match in matches) {
+                    var matchText = match.Value;
+                    var matchParameters = match.Groups[1].Value.Split(",").Select(x => x.Trim()).ToList();
+
+                    var newBody = body;
+                    for (int i = 0; i < parameters.Count; i++) {
+                        newBody = newBody.Replace(parameters[i], matchParameters[i]);
+                    }
+
+                    text = text.Replace(matchText, newBody);
+                }
+            }
+
+            File.WriteAllText("macro_" + path, text);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharpMacros
+ Bringing Macros to CSharp, primarily for Cosmos to prevent method calling performance overhead
