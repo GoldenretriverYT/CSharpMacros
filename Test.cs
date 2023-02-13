b701271883cc250582bf35b568a9af053455e3d2
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-02-13 21:48:05 +0100

    Create Test.cs

---
+++ Test.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Test {
+    internal class Program {
+        /*macro Plus x y
+        (x+y).ToString()
+            */
+
+        /*macro GetNumberString aValue
+        string[] xChars = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        xResult = string.Empty;
+
+        if (aValue == 0)
+        {
+            xResult = "0";
+        }
+        else
+        {
+            int xValue = aValue;
+
+            if (aValue < 0)
+            {
+                xValue *= -1;
+            }
+
+            while (xValue > 0)
+            {
+                int xValue2 = xValue % 10;
+                xResult = string.Concat(xChars[xValue2], xResult);
+                xValue /= 10;
+            }
+        }
+
+        if (aValue < 0)
+        {
+            xResult = string.Concat("-", xResult);
+        }
+        */
+        static void Main(string[] args) {
+            string xResult; // dummy variable to prevent IDE from complaining about missing variable
+            GetNumberString(123);
+
+            Console.WriteLine(xResult);
+            // as our macro returns a string, this will be a string concatenation and not a numeric addition
+            Console.WriteLine(Plus(1, 2) + Plus(3, 4));
+        }
+
+        #region MacroDummies
+        // Dummy method required for IDE to not complain about missing method
+        static string Plus(int x, int y) => {return "";}
+    static string GetNumberString(int x) => {return "";}
+        #endregion
+    }
+}
